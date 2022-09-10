0333be0f5afc5c85fad5874495248371028d4239
Aaron Zomback <aaronzomback@protonmail.com>
2022-09-10 13:39:41 +0200

    feat: set user-list as default route

---
+++ convelio/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+
+const routes: Routes = [
+  { path: 'user-list', component: UsersComponent },
+  { path: '', redirectTo: '/user-list', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
