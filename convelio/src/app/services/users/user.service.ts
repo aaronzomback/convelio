5a2f5c917e8a85cca87d6f782a351aebb3c14e8f
Aaron Zomback <aaronzomback@protonmail.com>
2022-09-10 16:02:59 +0200

    feat: fetch and display users async from endpoint

---
+++ convelio/src/app/services/users/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { User } from 'src/app/users/types/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  getUsers(): Observable<User[]> {
+    return this.http
+      .get<User[]>(this.usersUrl)
+      .pipe(catchError(this.handleError<User[]>('getUsers', [])));
+  }
+
+  constructor(private http: HttpClient) {}
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
--- convelio/src/app/users/users.component.ts
+++ convelio/src/app/users/users.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from './types/user';
+import { UserService } from '../services/users/user.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  users: User[] = [];
+  constructor(private userService: UserService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getUsers(): void {
+    this.userService.getUsers().subscribe(users => (this.users = users));
   }
-
+  ngOnInit(): void {
+    this.getUsers();
+  }
 }
